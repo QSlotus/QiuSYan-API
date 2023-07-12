41d436a3734aa8005725c170779feff2ca9da5d4
QiuSYan <halo-402@outlook.com>
2023-07-13 07:54:03 +0800

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>QiuSYan's API</title>
+  </head>
+  <body>
+    <?php echo '<p>QiuSYans&#39;API</p>'; ?> 
+
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+
+  </body>
+</html>
