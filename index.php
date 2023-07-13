6505180480c1ae91555f548033584b39b6daf73c
QiuSYan <halo-402@outlook.com>
2023-07-13 09:43:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <meta charset='UTF-8'>
     <meta name='viewport' content='width=device-width initial-scale=1'>
     <link href='https://fonts.loli.net/css?family=Open+Sans:400italic,700italic,700,400&subset=latin,latin-ext' rel='stylesheet' type='text/css' />
-    <link href='assets/css/typora.css'>
+    <link href='assets/css/typora.css' rel='stylesheet'>
     <title>QiuSYan's API</title>
   </head>
   <body class='typora-export os-windows'>
-    <?php echo '<div id='write'  class=''><h1><a name="qiusyans-api" class="md-header-anchor"></a><span>QiuSYan&#39;s API</span></h1><p><strong><span>目前可用API有：</span></strong></p><ul><li><a href='api.php'><span>随机一言</span></a></li></ul><p>&nbsp;</p></div>'; ?> 
-
+    <?php echo "<div id='write' class=''><h1><a name='qiusyans-api' class='md-header-anchor'></a><span>QiuSYan&#39;s API</span></h1><p><strong><span>目前可用API有：</span></strong></p><ul><li><a href='api.php'><span>随机一言</span></a></li></ul><p>&nbsp;</p></div>"; ?> 
   </body>
 </html>
