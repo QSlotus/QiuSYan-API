0daece90a889a973498365616b5103a5cbbb95de
QiuSYan <halo-402@outlook.com>
2023-07-13 08:12:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,5 @@
   <body>
     <?php echo '<p>QiuSYans&#39;API</p>'; ?> 
 
-  <!--
-  This script places a badge on your repl's full-browser view back to your repl's cover
-  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
-  teal, blue, blurple, magenta, pink!
-  -->
-
   </body>
 </html>
