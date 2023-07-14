62535655e99d425584c8328ced8e769a32766eec
QiuSYan <yhxiloveu@gmail.com>
2023-07-14 08:54:25 +0800

    update ip.php

--- ip.php
+++ ip.php
@@ -1,4 +1,9 @@
 <?php
-$ip = $_SERVER["REMOTE_ADDR"];
-echo $ip . "\n";
+if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+  $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+  $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+echo "Client IP: " . $ip;
 ?>
