44f2cd5249452762895c95527c9f5206f1e3c9cb
QiuSYan <yhxiloveu@gmail.com>
2023-07-14 08:12:39 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# YiYan-php
+# QiuSYan的自建API
+
+## YiYan-php
 
 php简单实现随机一言，项目部署于Vercel。
 <br>
 数据文件是`data.dat`，欢迎PR！
+
+## 持续更新中...
--- yiyan.php
+++ yiyan.php
@@ -1,6 +1,6 @@
 <?php
 // 存储数据的文件
-$filename = 'data.dat';        
+$filename = 'data/yiyan-data.dat';        
  
 // 指定页面编码
 header('Content-type: text/html; charset=utf-8');
